86714d77dc39b4d68b2cb4e2526e072d0de4aba6
Minamo <kleinminamo@gmail.com>
2017-09-12 17:36:02 +0800

    init rpg

---
+++ Assets/Scripts/MaskAlphaScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 
+ * 遮罩透明
+ * Weiss
+ * 
+ * */
+public class MaskAlphaScript : MonoBehaviour {
+
+    private float alpha = 0.0f;
+    private float alphaSpeed = 1.0f;
+
+    private CanvasGroup cg;
+
+    void Start()
+    {
+        cg = this.transform.GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        if (alpha != cg.alpha)
+        {
+            cg.alpha = Mathf.Lerp(cg.alpha, alpha, alphaSpeed * Time.deltaTime);
+            if (Mathf.Abs(alpha - cg.alpha) <= 0.01)
+            {
+                cg.alpha = alpha;
+            }
+        }
+    }
+
+    public void Show()
+    {
+        alpha = 1;
+
+        cg.blocksRaycasts = true;//可以和该UI对象交互    
+    }
+
+    public void Hide()
+    {
+        alpha = 0;
+
+        cg.blocksRaycasts = false;//不可以和该UI对象交互    
+    }
+}
---
+++ Assets/Scripts/MovieCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieCamera : MonoBehaviour {
+    float speed = 10;
+    float endZ = -20;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z < endZ)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+	}
+}
