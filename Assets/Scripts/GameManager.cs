aba79517e6acd171020f4e3d8dcf44634d26b8b1
Minamo <kleinminamo@gmail.com>
2017-09-21 19:23:17 +0800

    Add character creation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public void NewGame (string sceneName) {
+        PlayerPrefs.SetInt("DataFromSave", 0);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadGame()
+    {
+        PlayerPrefs.SetInt("DataFromSave", 1);
+    }
+}
---
+++ Assets/Scripts/character creation/characterCreation.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterCreation : MonoBehaviour {
+    public GameObject[] characterPrefabs;
+    private GameObject[] characterGameObects;
+    private int selectedIndex = 0;
+    private int length;
+
+    public InputField nameInput;
+
+    // Use this for initialization
+    void Start () {
+        length = characterPrefabs.Length;
+        characterGameObects = new GameObject[length];
+        for(int i = 0; i < length; i++)
+        {
+            characterGameObects[i] = GameObject.Instantiate(characterPrefabs[i], transform.position, transform.rotation) as GameObject;
+        }
+        UpdateCharacterShow();
+    }
+
+    //更新所有角色的显示
+    void UpdateCharacterShow()
+    {
+        characterGameObects[selectedIndex].SetActive(true);
+        for (int i = 0; i < length; i++)
+        {
+            if (i != selectedIndex)
+            {
+                characterGameObects[i].SetActive(false);
+            }
+        }
+    }
+
+    public void OnNextButtonClick()
+    {
+        selectedIndex++;
+        selectedIndex %= length;
+        UpdateCharacterShow();
+    }
+
+    public void OnPrevButtonClick()
+    {
+        selectedIndex--;
+        if(selectedIndex == -1)
+        {
+            selectedIndex = length - 1;
+        }
+        UpdateCharacterShow();
+    }
+
+    public void OnOkButtonClick()
+    {
+        PlayerPrefs.SetInt("selectedCharacterIndex", selectedIndex);
+        PlayerPrefs.SetString("name", nameInput.text);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
